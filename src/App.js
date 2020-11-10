6f0a6ee6da8275750ba7b8dd82c4a4ee7e9a8c90
Ed Keric <Ede@Joscha.fritz.box>
2020-11-10 17:20:30 +0100

    make carousel component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Fragment} from "react";
 import './App.css';
 import { Switch, Route, NavLink, useParams, useRouteMatch } from "react-router-dom";
 import DetailedRecipe from "./DetailedRecipe"
+import { Container, Row, Col, Carousel} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 var contentful = require('contentful')
 
 function App() {
@@ -27,11 +29,65 @@ function App() {
   let { path } = useRouteMatch();
 
   return (
-    <div className="App">
-
-     <h1> Hello dreamteam</h1>
+    <Container style={{backgroundColor: "skyblue"}}>
+    
+     <Row>   
+    <Col>HOME</Col>
+    <Col>ABOUT</Col>
+    <Col>CONTACT</Col>
+    </Row>
+  
+      
    
-{/*loop over recipes array and make a list entry for each recipe name with a link to the recipe page. each li-tag needs individual key*/}
+
+
+    <Carousel>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100"
+      src="http://www.erlebnis-schweiz.com/blog/wp-content/uploads/Burger.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+    <img
+      className="d-block w-100"
+      src="https://www.jocooks.com/wp-content/uploads/2011/09/french-toast-1-2-1-768x960.jpg"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://i1.wp.com/nonnasway.com/wp-content/uploads/2015/04/vegetable-penne-dish.jpg?w=714&ssl=1"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
+
+
+
+  
+
+    
+   
+
+ 
+
+
 
       <nav>
       {recipes.map((recipe) => (
@@ -59,7 +115,9 @@ function App() {
             </Route>
           </Switch>
 
-    </div>
+     
+
+          </Container>
   );
 }
 
