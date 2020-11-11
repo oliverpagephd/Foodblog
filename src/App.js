301e8dc7b17f404b738e7ee70efcac261624a61f
Svenja Niehus <svenja.niehus@gmail.com>
2020-11-11 18:19:42 +0100

    screwing things up

--- src/App.js
+++ src/App.js
@@ -7,83 +7,76 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 var contentful = require('contentful')
 
 function App() {
-
+  
   const [recipes, setRecipes]=useState([])
   //Initialising and fetching recipes once upon first loading of page
   useEffect(()=>{
-  //Initialising the client
+    //Initialising the client
     var client = contentful.createClient({
       space: process.env.REACT_APP_CONTENTFUL_SPACE,
       accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_KEY
     })
-  //Instead of using fetch; is incorporated in contenful
+    //Instead of using fetch; is incorporated in contenful
     client.getEntries()
     .then(function (entries) {
       entries.items.forEach(function (entry) {
-            console.log(entry)
+        console.log(entry)
       })
       setRecipes(entries.items)
     })
   },[])
-
+  
   let { path } = useRouteMatch();
-
+  
   return (
     <Container style={{backgroundColor: "skyblue"}}>
     
-     <Row>   
+    <Row>   
     <Col>HOME</Col>
     <Col>ABOUT</Col>
     <Col>CONTACT</Col>
     </Row>
-
-
-    <Carousel>
+    
+    
+    <Switch>
+    <Route exact path="/">
+    <nav>
+    {recipes.map((recipe) => (
+      <li key={recipe.sys.id}>
+      <NavLink to={`${path}${recipe.sys.id}`}>
+      {recipe.fields.title}
+      </NavLink>
+      </li>))}
+      </nav>
+      
+      <Carousel>
       {recipes.map((recipe) => (
-        <Carousel.Item interval={3000}>
-          <img
-            className="d-block w-100"
-            src={`http:${recipe.fields.image.fields.file.url}`}
-            alt={`Picture of ${recipe.fields.title}`}
-          />
-          <Carousel.Caption>
-            <h3>{recipe.fields.title}</h3>
-          </Carousel.Caption>
+        <Carousel.Item interval={2000}>
+        <img
+        className="d-block w-100"
+        key={recipe.sys.id}
+        src={`http:${recipe.fields.image.fields.file.url}`}
+        alt={`Picture of ${recipe.fields.title}`}
+        />
+        <Carousel.Caption>
+        <h3>{recipe.fields.title}</h3>
+        </Carousel.Caption>
         </Carousel.Item>
-      ))}
-    </Carousel>
-
-
-      <nav>
-      {recipes.map((recipe) => (
-        <li key={recipe.sys.id}>
-          <NavLink to={`${path}${recipe.sys.id}`}>
-          {recipe.fields.title}
-          </NavLink>
-        </li>))}
-      </nav> 
-
-      <Switch>
-            <Route exact path="/">
-              <nav>
-                {recipes.map((recipe) => (
-                  <li key={recipe.sys.id}>
-                    <NavLink to={`${path}${recipe.fields.title}`}>
-                    {recipe.fields.title}
-                    </NavLink>
-                  </li>))}
-              </nav>
-            </Route>
-            <Route path="/:id">
-             <p>i'm a recipe!</p>
-              <DetailedRecipe recipes={recipes} />
-            </Route>
+        ))}
+        </Carousel>
+        
+        </Route>
+        <Route path="/:id" render={props => (
+          <DetailedRecipe recipes={recipes} {...props} />
+          )}/>
+          
           </Switch>
-
-     
-
+          
+          
+          
           </Container>
-  );
-}
-
-export default App;
+          );
+        }
+        
+        export default App;
+        
--- src/DetailedRecipe.js
+++ src/DetailedRecipe.js
@@ -1,49 +1,34 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
+import './DetailedRecipe.css'
+import { Container, Row, Col, Image, Card} from "react-bootstrap";
 import { Switch, Route, NavLink, useParams, useRouteMatch } from "react-router-dom";
 var contentful = require('contentful')
 
 export default function DetailedRecipe(props){ 
     
     const {id} = useParams();
-    
 
-  const [detailedRecipeTitle, setDetailedRecipeTitle]=useState()
-  const [detailedRecipeDescription, setDetailedRecipeDescription]=useState()
-  const [detailedRecipeInstructions, setDetailedRecipeInstructions]=useState()
-  const [detailedRecipeUrl, setDetailedRecipeUrl]=useState()
-  const [detailedRecipeIngredients, setDetailedRecipeIngredients]=useState()
+  const [recipe, setRecipe]=useState()
 
-  const getRecipe = async () => {
-    const recipe = await props.recipes.find(recipe =>  id === recipe.sys.id)
-    setDetailedRecipeTitle(recipe.fields.title)
-    setDetailedRecipeDescription(recipe.fields.description)
-    setDetailedRecipeInstructions(recipe.fields.instructions)
-    setDetailedRecipeUrl(recipe.fields.image.fields.file.url)
-    setDetailedRecipeIngredients(recipe.fields.ingriedients)
-    console.log(recipe)
-    
-}
-   
-    useEffect(async()=>{ 
-        await getRecipe()
+
+  const getRecipe = () => {
+    console.log(id)
+    console.log(props.recipes)
+    const recipe = props.recipes.find(recipe =>  id === recipe.sys.id)
+    setRecipe(recipe)
+    console.log(recipe)  
+  }
+
+    useEffect(()=>{ 
+        getRecipe()
     },[id])
 
     
     //destructuring
     //const {recipes} = props 
 
-    return (
-        <>  
-       
-       <p>{detailedRecipeTitle}</p>
-       <p>{detailedRecipeDescription}</p>
-       <p>{detailedRecipeInstructions}</p>
-       <img src={`http:${detailedRecipeUrl}`}></img>
-       <p>{"I'm the Ingredients!" + {detailedRecipeIngredients}}</p> 
-       {console.log("hi")}
-       {console.log(setDetailedRecipeIngredients)}
-                  
-        </>
-    )
+    return recipe ? ("its here")  : ("its not here yet :O")
+    
+    
 }
